129b49da376fb6fade7fbfd977fc8aff1835bb77
Cet <korlyakov.2001@mail.ru>
2021-05-26 18:57:50 +1000

    update template

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, request, send_from_directory
 from app import app, db
 
 # ------------------------ main pages ------------------------ #
@@ -6,4 +6,14 @@ from app import app, db
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello World!"
+	return render_template( "index.html", title = "Главная" )
+
+
+# ------------------------ technical pages ------------------------ #
+
+
+@app.route('/favicon.ico')
+@app.route('/robots.txt')
+@app.route('/sitemap.xml')
+def static_from_root():
+	return send_from_directory(app.static_folder, request.path[1:])
